bf0bf24e2fcfcd5a9adde0ecb11a76d610161c20
aarogyaojha <arogyaojha251@gmail.com>
2026-03-05 05:37:12 +0545

    chore: updated server/middlewares/auth/adminAuth.js

--- server/middlewares/auth/adminAuth.js
+++ server/middlewares/auth/adminAuth.js
@@ -4,7 +4,7 @@ const Admin = require("../../models/admin.model");
 const requireAdminAuth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const admin = await Admin.findById(decoded.id);
 
