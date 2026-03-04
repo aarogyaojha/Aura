2df06178066d277794d4b77d30912d292a8d6b85
aarogyaojha <arogyaojha251@gmail.com>
2026-03-05 05:37:12 +0545

    chore: updated server/middlewares/auth/decodeToken.js

--- server/middlewares/auth/decodeToken.js
+++ server/middlewares/auth/decodeToken.js
@@ -9,7 +9,7 @@ const decodeToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader.split(" ")[1];
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
     next();
   } catch (err) {
